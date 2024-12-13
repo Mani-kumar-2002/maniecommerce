2c404829c1f20407b6574385f54603645909ef95
Mani-kumar-2002 <manikumar6423@gmail.com>
2024-12-13 19:56:21 +0530

    mess

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['https://mern-ecommerce-2024-xufp.vercel.app', 'http://localhost:5173'],
+  origin: ['https://manikumarecommerce.vercel.app/', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
