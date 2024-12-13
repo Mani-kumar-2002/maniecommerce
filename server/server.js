07b81d0e2fe9f0d9b322fe066dd130283bb44190
Mani-kumar-2002 <manikumar6423@gmail.com>
2024-12-14 00:30:40 +0530

    xncbfv

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['https://manikumarecommerce.vercel.app/', 'http://localhost:5173'],
+  origin: ['https://maniecommerce-kw5j.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
